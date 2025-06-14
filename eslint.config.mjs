5a45cfdcdf27a1510857f7984f5c62e26dba39fd
ivanrozak <ivanrozack@gmail.com>
2025-06-14 23:30:38 +0700

    setup base nextjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,42 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import antfu from '@antfu/eslint-config';
+import nextPlugin from '@next/eslint-plugin-next';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tailwind from 'eslint-plugin-tailwindcss';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
-
-export default eslintConfig;
+export default antfu(
+  {
+    typescript: true,
+    react: true,
+    stylistic: {
+      indent: 2,
+      quotes: 'single',
+      semi: true,
+    },
+    jsonc: false,
+    yaml: false,
+    ignores: ['node_modules', '.next', 'public', 'out', '*.d.ts'],
+  },
+  {
+    plugins: {
+      '@next/next': nextPlugin,
+      'tailwindcss': tailwind,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...nextPlugin.configs.recommended.rules,
+      ...nextPlugin.configs['core-web-vitals'].rules,
+      'tailwindcss/classnames-order': 'off', // Disabled for performance
+      'tailwindcss/no-custom-classname': 'off',
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+    },
+    settings: {
+      tailwindcss: {
+        config: './tailwind.config.ts',
+        cssFiles: ['./src/app/**/*.css'],
+      },
+    },
+  },
+);
